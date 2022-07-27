faa0f389464ee5794645d9611388017a0cfae096
Anusha <anushavysakh232@gmail.com>
2022-07-27 17:52:06 +0530

    Uc11 committed

---
+++ EmployeeWage/EmpwageBuilderinterface.java
@@ -0,0 +1,8 @@
+package com.Assignment.Bridlabz;
+
+public interface IComputeEmpWage {
+	 public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+	 public void computeWage();
+	//  public int getTotalWage(String company);
+
+}
