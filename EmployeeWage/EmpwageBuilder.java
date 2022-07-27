2eed7d96c6a3f745a12f5e67732bf870ed572572
Anusha <anushavysakh232@gmail.com>
2022-07-27 17:29:55 +0530

    First commit

---
+++ EmployeeWage/EmpwageBuilder.java
@@ -0,0 +1,7 @@
+package com.Assignment.Bridlabz;
+
+public class EmpwageBuilder {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+}
