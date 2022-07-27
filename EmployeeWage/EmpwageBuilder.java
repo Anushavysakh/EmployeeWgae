e07a700e85a9a202b191f9fa6cbc444f25201482
Anusha <anushavysakh232@gmail.com>
2022-07-27 17:36:00 +0530

    Uc2 committed

--- EmployeeWage/EmpwageBuilder.java
+++ EmployeeWage/EmpwageBuilder.java
@@ -1,19 +1,25 @@
 package com.Assignment.Bridlabz;
 
-import java.util.Random;
-
-public class Uc1 {
-
+public class uc2 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int IS_FULL_TIME = 1;
-
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee Present..");
+			empHrs = 8;
 		} else {
-			System.out.println("Employee is Absent");
+			System.out.println("Employee Absent..");
+			empHrs = 0;
 		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Daily wage of employee =  " + empWage);
+		
 	}
 }
