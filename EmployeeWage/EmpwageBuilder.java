98234b16480646bff7ece0d2bcb5833f5efb1bc6
Anusha <anushavysakh232@gmail.com>
2022-07-27 17:34:41 +0530

    Uc1 committed

--- EmployeeWage/EmpwageBuilder.java
+++ EmployeeWage/EmpwageBuilder.java
@@ -1,7 +1,19 @@
 package com.Assignment.Bridlabz;
 
-public class EmpwageBuilder {
-	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+import java.util.Random;
+
+public class Uc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int IS_FULL_TIME = 1;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
